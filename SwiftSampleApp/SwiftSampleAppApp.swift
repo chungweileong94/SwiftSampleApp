c2fd82476903fb2ff00e2ae7c92417aa4f6a1fb9
Chung Wei <leongchungwei@hotmail.com>
2023-07-02 20:37:35 +0800

    Initial Commit

---
+++ SwiftSampleApp/SwiftSampleAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftSampleAppApp.swift
+//  SwiftSampleApp
+//
+//  Created by Chung Wei Leong on 02/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftSampleAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
